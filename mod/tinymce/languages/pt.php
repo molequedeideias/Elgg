86f3915c873447fc680676973f84a86c3ff0c01d
Eric Vieira de Souza <eric@moleque.com.br>
2012-11-27 17:13:16 -0200

    add pt localization in tinymce plugin

---
+++ mod/tinymce/languages/pt.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * TinyMCE language pack.
+ *
+ * @package ElggTinyMCE
+ */
+
+return array(
+	'tinymce:remove' => "Remover editor",
+	'tinymce:add' => "Adicionar editor",
+	'tinymce:word_count' => 'Contagem de palavras: ',
+	'tinymce:lang_notice' => "Your site language is %s but it isn't installed for TinyMCE. Get it <a target=\"_blank\" href=\"%s\">here</a> and copy it to %s. Then, <a href=\"%s\">flush the caches</a>. See the TinyMCE README for more details.",
+);
